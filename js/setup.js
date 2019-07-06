06a620df86b35845c6cddc06acafea3f4d7fbfe9
Dima Malishenko <MalishenkoDV@gmail.com>
2019-07-06 12:34:02 +0300

    adding listeners

--- js/setup.js
+++ js/setup.js
@@ -2,20 +2,17 @@
 
 var NUMBER_WIZARDS = 4;
 var NAME_MASS = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var SURNAME_MASS = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго'];
+var SURNAME_MASS = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвин'];
 var COATS_MASS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYESCOLOR_MASS = ['black', 'red', 'blue', 'yellow', 'green'];
+var FIREBALL_MASS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
 /**
- * Показываем окно с параметрами мага
+ * Функция по произвольной выборке числа
+ * @param {number} min - указывает начало диапазона цифр
+ * @param {number} max - указывает конец диапазона цифр
+ * @return {*} - переводит диапазон в начальную и конечную точки и возвращает произвольный результат.
  */
-var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
-
-var similarWizardTemplate = document.querySelector('#similar-wizard-template')
-    .content
-    .querySelector('.setup-similar-item');
-
 var getRandomFromInterval = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
@@ -30,7 +27,7 @@ var getRandomElementFromArray = function (arr) {
 };
 
 /**
- * Созданик объекта "Волшебник"
+ * Создание объекта "Волшебник"
  * @param {str} name - свойство "Волшебника" - полное имя, сложенное из массива имен и фамилий
  * @param {str} coatColor - свойство "Волшебника" - цвет курточки
  * @param {str} eyes - свойство "Волшебника" - цвет глаз
@@ -52,8 +49,16 @@ for (var i = 0; i < NUMBER_WIZARDS; i++) {
 }
 
 /**
+ * Поиск в темплейтах по ид
+ * эллемента с нужным классом
+ */
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+    .content
+    .querySelector('.setup-similar-item');
+
+/**
  * Клонирование волшебников
- * @param {object} wizardObject -- обьект данных волшебников
+ * @param {object} wizardObject -- обьект данных волшебника
  * @return {object} wizardElement
  */
 var renderWizard = function (wizardObject) {
@@ -73,6 +78,60 @@ wizardObjects.forEach(function (wizard) {
   fragment.appendChild(renderWizard(wizard));
 });
 
+/**
+ * Показываем окно с параметрами мага
+ */
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+
 var similarListElement = userDialog.querySelector('.setup-similar-list');
 similarListElement.appendChild(fragment);
 document.querySelector('.setup-similar').classList.remove('hidden');
+
+/* Задание 4.1 */
+
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
+
+// Нажатие на элемент .setup-open удаляет класс hidden
+// у блока setup. Нажатие на элемент .setup-close, расположенный
+// внутри блока setup возвращает ему класс hidden.
+var setup = document.querySelector('.setup');
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = setup.querySelector('.setup-close');
+
+var onPopupEscPress = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closePopup();
+  }
+};
+
+var openPopup = function () {
+  setup.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var closePopup = function () {
+  setup.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+setupOpen.addEventListener('click', function () {
+  openPopup();
+});
+
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openPopup();
+  }
+});
+
+setupClose.addEventListener('click', function () {
+  closePopup();
+});
+
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closePopup();
+  }
+});
